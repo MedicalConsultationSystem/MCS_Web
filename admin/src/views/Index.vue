37c82ea252e78d42a9155e6f69e64fbd9fc8ef76
Stephen <31801162@stu.zucc.edu.cn>
2021-07-13 20:35:43 +0800

    清除之前因为尝试添加的组件

--- admin/src/views/Index.vue
+++ admin/src/views/Index.vue
@@ -1,27 +1,10 @@
 <template>
   <div class="index">
-    <Header></Header>
-    <LeftMenu></LeftMenu>
-    <div class="rightContainer" :class="{'content-collapse':collapse}">
-      <Tags/>
-      <div class="content">
-        <transition name="move" mode="out-in">
-          <keep-alive :include="tagsList">
-            <router-view></router-view>
-          </keep-alive>
-        </transition>
-      </div>
-    </div>
+
   </div>
 </template>
 
 <script>
-import Header from "../components/Header";
-import LeftMenu from "../components/LeftMenu";
-import bus from "../common/bus";
-import Tags from "../common/Tags";
-// import moment from 'moment'
-// @ is an alias to /src
 export default {
   name: "Index",
   data() {
@@ -30,27 +13,6 @@ export default {
       collapse: false
     };
   },
-  components: {
-    Header,
-    LeftMenu,
-    Tags
-  },
-  created() {
-    //内容区域跟随变化
-    bus.$on("collapse", msg => {
-      console.log(msg);
-      this.collapse = msg;
-    }),
-        // 只有在标签页列表里的页面才使用keep-alive，即关闭标签之后就不保存到内存中了。
-        bus.$on("tags", msg => {
-          let arr = [];
-          for (let i = 0, len = msg.length; i < len; i++) {
-            msg[i].name && arr.push(msg[i].name);
-          }
-          this.tagsList = arr;
-          // console.log(tags)
-        });
-  }
 };
 </script>
 <style scoped>
