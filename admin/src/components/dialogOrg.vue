ee76ae8cd419e86f7b82046997c6f4d6d68a6106
Stephen <31801162@stu.zucc.edu.cn>
2021-07-14 20:43:48 +0800

    添加机构界面

---
+++ admin/src/components/dialogOrg.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-dialog :title="dialog.title" :visible.sync="dialog.show">
+    <el-form :model="formData">
+      <el-form-item label="机构名称" :label-width="formLabelWidth">
+        <el-input v-model="formData.orgName" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialog.show = false">取 消</el-button>
+      <el-button type="primary" @click="dialog.show = false">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: "Dialog",
+  data(){
+    return{
+      formLabelWidth: '120px',
+    }
+  },
+
+  props:{
+    dialog:Object,
+    formData:Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- admin/src/views/org/Org.vue
+++ admin/src/views/org/Org.vue
@@ -1,22 +1,196 @@
 <template>
-  <div>
-    <text>
-      {{ la }}
-    </text>
+  <div class="toollist">
+    <div>
+      <el-form :inline="true" ref="search_data" :model="search_data">
+        <div class="search">
+          <el-form-item>
+            <el-input
+                v-model="searchData"
+                placeholder="请输入机构姓名">
+            </el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" size ="small" icon="el-icon-search">查询</el-button>
+          </el-form-item>
+        </div>
+        <el-form-item class="btnRight">
+          <el-button type="primary" size ="small" icon="el-icon-edit-outline" @click="onAddDoctor()" >添加</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="table_container">
+      <el-table
+          v-if="tableData.length > 0"
+          :data="tableData"
+          min-height="460px"
+          border
+          :default-sort = "{prop: 'date', order: 'descending'}"
+          style="width: 100%">
+        <el-table-column
+            label="机构名称"
+            align="center"
+        >
+          <template slot-scope="scope">
+            <span>{{ scope.row.orgName }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+            align="center"
+            label="操作">
+          <template slot-scope="scope">
+            <el-button
+                type="primary"
+                size="small"
+                @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button
+                size="small"
+                type="danger"
+            >删除</el-button>
+            <!-- <el-popover
+              placement="top"
+              width="160"
+              v-model="visible">
+              <p>确定要删除吗？</p>
+              <div style="text-align: right; margin: 0">
+                <el-button size="mini" type="text" @click="visible = false">取消</el-button>
+                <el-button type="primary" size="mini" @click="getDeleteVisible(scope.$index, scope.row)">确定</el-button>
+              </div>
+              <el-button slot="reference" @click="handleDelete(scope.row)">删除</el-button>
+            </el-popover> -->
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <Dialog :dialog="dialog" :form-data="formData"></Dialog>
   </div>
 </template>
 
 <script>
+import Dialog from "@/components/dialogOrg";
 export default {
   name: "Org",
+  components: {Dialog},
   data() {
     return {
-      la: "hello",
+      search_data:{
+        doctorName:'',
+      },
+      message:"数据不存在",
+      formLabelWidth: '120px',
+      paginations:{
+        page_index:1, //当前位于哪页
+        total:0, //总数
+        page_size:10, //一页显示多少条
+        page_sizes:[10,15,20], //每页显示多少条
+        layout:'total, sizes, prev, pager, next, jumper'
+      },
+      allTableData:[
+        {
+          orgName:"浙江省第一医院"
+        },
+        {
+          orgName:"浙江医院"
+        },
+        {
+          orgName:"杭州市中医院"
+        },
+        {
+          orgName:"浙江省人民医院浙江省立医院"
+        }
+      ],
+      dialog:{  //弹出框
+        title:'',
+        show:false,
+        option:'edit'
+      },
+      tableData:[], //分页数据
+      formData : {
+        orgName:""
+      }
     }
+  },
+  created () {
+    this.setPaginations();
+  },
+  methods:{
+    handleCurrentChange(page){
+      //获取当前页
+      let index = this.paginations.page_size * (page -1);
+
+      //数据的总数
+      let nums = this.paginations.page_size * page;
+      //容器
+      let tables = [];
+      for(let i = index; i < nums; i++) {
+        if(this.allTableData[i]) {
+          tables.push(this.allTableData[i])
+        }
+        this.tableData = tables;
+      }
+    },
+    onAddDoctor(){//添加信息
+      this.dialog={
+        title:'编辑信息',
+        show:true,
+        option:'edit'
+      }
+      this.formData = {
+        orgName:""
+      }
+    },
+    handleSizeChange(page_size){
+      this.paginations.page_index = 1;
+      this.paginations.page_size = page_size;
+      this.tableData = this.allTableData.filter((item,index) => {
+        return index < page_size;
+      })
+    },
+    setPaginations(){
+      //分页属性设置
+      this.paginations.total = this.allTableData.length; //数据的数量
+      this.paginations.page_index = 1; //默认显示第一页
+      this.paginations.page_size = 10; //每页显示的数据
+      //数据显示
+      this.tableData = this.allTableData.filter((item,index) => {
+        return index < this.paginations.page_size;
+      })
+
+    },
+    handleEdit(index,row) { //编辑信息
+      this.dialog={
+        title:'编辑信息',
+        show:true,
+        option:'edit'
+      }
+      console.log("lalal")
+      this.formData = {
+        orgName:row.orgName
+      }
+    },
   }
 }
 </script>
 
 <style scoped>
+.toollist {
+  padding: 10px;
+}
+.search{
+  float: left;
+}
+.btnRight{
+  float: right;
+}
+.paginations{
+  text-align: right;
+  margin-top: 10px;
+}
+.txt {
+  padding: 20px 0;
+  background: #eee;
+  font-size: 16px;
+  text-align: center;
+  color: #606266;
+}
 
 </style>
