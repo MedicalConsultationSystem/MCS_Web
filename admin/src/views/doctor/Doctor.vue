69654ec1c2bafb1893b54e77fe1d8ddfad5c3289
Stephen <31801162@stu.zucc.edu.cn>
2021-07-14 09:49:38 +0800

    添加查询，新增组件

--- admin/src/views/doctor/Doctor.vue
+++ admin/src/views/doctor/Doctor.vue
@@ -1,13 +1,51 @@
 <template>
-<div>i am doctor</div>
+<div class="toollist">
+  <div>
+    <el-form :inline="true" ref="search_data" :model="search_data">
+      <div class="search">
+        <el-form-item label="请选择时间">
+          <el-input
+              v-model="name"
+              placeholder="请输入医生姓名">
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size ="small" icon="el-icon-search">查询</el-button>
+        </el-form-item>
+      </div>
+      <el-form-item class="btnRight">
+        <el-button type="primary" size ="small" icon="el-icon-edit-outline" >添加</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-name: "Doctor"
+name: "Doctor",
+  data(){
+  return{
+    visible: false,
+    message:"数据不存在",
+    search_data:{
+      startDate:'',
+      endDate:''
+    }
+  }
+  }
 }
 </script>
 
 <style scoped>
+.toollist {
+  padding: 10px;
+}
+.search{
+  float: left;
+}
+.btnRight{
+  float: right;
+}
 
 </style>
